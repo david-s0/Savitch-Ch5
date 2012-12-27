9d74971ef03bd8a0df38629195ad8f505fd3e401
david-s0 <ds2412@imperial.ac.uk>
2012-12-27 15:26:57 +0000

    I need to review enums!

---
+++ ex16/main.cpp
@@ -0,0 +1,13 @@
+#include "yearFuncs.hpp"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int day, month, year;
+
+  getInput(day, month, year);
+  postOutput(day, month, year);
+  return 0;
+}
---
+++ ex16/yearFuncs.hpp
@@ -0,0 +1,20 @@
+//checks if a year is a leap year
+bool isLeapYear(int year);
+
+//calculate the century value
+int getCenturyValue(int year);
+
+//calculate the year value
+int getYearValue(int year);
+
+//calculate the month value
+int getMonthValue(int month, int year);
+
+//calculate the day
+int getDayValue(int day, int month, int year);
+
+//receive input from user
+void getInput(int& day, int& month, int& year);
+
+//output the result
+void postOutput(int& day, int& month, int& year);
